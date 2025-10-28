6dfe0659dba2f08c379f4b8d91f00df9b11ce346
harshitXartian <harshit.26sachan@gmail.com>
2025-10-28 22:06:25 +0530

    Cli added

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
- 
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:string_calculator_kata_task/application/string_calculator.dart';
+
+void main(List<String> args) async {
+  const calc = StringCalculator();
+
+  try {
+    final raw = args.isNotEmpty
+        ? args.join(' ')
+        : await stdin.transform(utf8.decoder).join();
+
+    final normalized = raw.replaceAll(r'\n', '\n');
+
+    final result = calc.add(normalized.trim());
+    stdout.writeln(result);
+  } catch (e) {
+    stderr.writeln(e);
+    exitCode = 1;
+  }
+}
